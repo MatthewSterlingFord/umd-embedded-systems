ffb2fbb2e721ebaf357679c80272781a86d35b55
Gcommer <gcommer@gmail.com>
2013-02-05 01:48:21 -0500

    Better comments for RIT

--- LedBlink_RIT/src/main.c
+++ LedBlink_RIT/src/main.c
@@ -1,10 +1,9 @@
 /*
 ===============================================================================
  Name        : main.c
- Author      : 
- Version     :
- Copyright   : Copyright (C) 
- Description : main definition
+ Author      : George Pittarelli
+ Description : Example of blinking an LED with the Repetitive Interrupt Timer
+              (RIT).
 ===============================================================================
 */
 
@@ -20,34 +19,41 @@
 // See crp.h header for more information
 __CRP const unsigned int CRP_WORD = CRP_NO_CRP ;
 
-// TODO: insert other include files here
-
-// TODO: insert other definitions and declarations here
-
 void RIT_IRQHandler(void) {
-	LPC_GPIO0->FIOPIN ^= (1 << 22);
-   // Clear RI Control register Interrupt
-   LPC_RIT->RICTRL |= 1<<0; //bit 0 = 1 => write 0 to value 1 will clear the interrupt flag
+	// Toggle LED pin
+	LPC_GPIO0->FIOPIN ^= 1 << 22;
+
+	// Clear RI Control register Interrupt
+	//   write 1 to bit 0 will clear the interrupt flag
+	LPC_RIT->RICTRL |= 1<<0;
 }
 
 int main(void) {
+	// Setup LED pin (using builtin LED on pin P0.22)
 	LPC_GPIO0->FIODIR = (1 << 22);
 	LPC_GPIO0->FIOSET = (1 << 22);
 
-	LPC_SC->PCONP |= 1 << 16;  // power
-	LPC_SC->PCLKSEL0 &= ~(2 << 26);
+	// Power for RIT clock
+	LPC_SC->PCONP |= 1 << 16;
+
+	// Choose clock source for RIT
+	// Writing `01` to bits 27:26 selects the CPU Clock, undivided
 	LPC_SC->PCLKSEL1 |= 1 << 26;
+
 	LPC_RIT->RICOMPVAL = SystemCoreClock / 2;
+
+	// Initialize RIT mask and counter (probably redundant)
 	LPC_RIT->RIMASK = 0;
 	LPC_RIT->RICOUNTER = 0;
 
-	//bit 1 = 1 => The timer will be cleared to 0 whenever the counter value equals RICOMPVAL
-	//bit 3 = 1 => Timer enable
+	// RIT control flags:
+	//  bit 1, the timer will be cleared to 0 whenever it equals RICOMPVAL
+	//  bit 3, enables timer
 	LPC_RIT->RICTRL = (1 << 1) | (1 << 3);
 
+	// Enable interrupt
 	NVIC_EnableIRQ(RIT_IRQn);
 
-	// Enter an infinite loop, just incrementing a counter
 	while(1);
 	return 0 ;
 }

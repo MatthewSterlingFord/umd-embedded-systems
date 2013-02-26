1dd0c6212f00e97895e6a4c309d951c64fbbc994
George Pittarelli <g.pittarelli@gmail.com>
2013-02-26 10:50:38 -0500

    First try at burst mode

--- AnalogRead_Int_Burst/src/main.c
+++ AnalogRead_Int_Burst/src/main.c
@@ -28,16 +28,19 @@ const char *HEX_CHAR_TABLE = "0123456789ABCDEF";
 
 uint_fast16_t analog_val;
 void ADC_IRQHandler(void) {
-  analog_val = (LPC_ADC ->ADDR0 >> 4) & 0x0fff;
-  alpha_display(HEX_CHAR_TABLE[analog_val >> 8]);
+  if (LPC_ADC ->ADDR0 & _BV(31)) {
+    //analog_val = (LPC_ADC ->ADDR0 >> 4) & 0x0fff;
+    //alpha_display(HEX_CHAR_TABLE[analog_val >> 8]);
 
-  // Print result
-  if ((CoreDebug->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk)) {
-    printf("%#X  %d\n", analog_val, analog_val);
+    // Print result
+    analog_val = (LPC_ADC ->ADDR0 >> 4) & 0x0fff;
+    if ((CoreDebug ->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk)) {
+      printf("%#X  %d\n", analog_val, analog_val);
+    }
+  } else if (LPC_ADC ->ADDR1 & _BV(31)) {
+    analog_val = (LPC_ADC ->ADDR1 >> 4) & 0x0fff;
+    alpha_display(HEX_CHAR_TABLE[analog_val >> 8]);
   }
-
-  // Start conversion
-  LPC_ADC ->ADCR |= _BV(24);
 }
 
 int main(void) {
@@ -51,11 +54,11 @@ int main(void) {
   // Set clock divider to 0+1=1
   LPC_SC ->CCLKCFG = 0;
 
-  // Power ADC
-  LPC_SC ->PCONP |= _BV(12);
+  // Power PWM, ADC
+  LPC_SC ->PCONP |= _BV(6) | _BV(12);
 
   // Choose undivided peripheral clocks for TIMER0,1
-  LPC_SC ->PCLKSEL0 |= (0 << 24);
+  //LPC_SC ->PCLKSEL0 |= (0 << 24);
 
   // Setup IO pins
   LPC_GPIO0 ->FIODIR = 0;
@@ -64,24 +67,27 @@ int main(void) {
 
   // Configure pins
   //   P0.23 as AD0.0
-  LPC_PINCON ->PINSEL1 |= (1 << 14);
+  //   P0.24 as AD0.1
+  LPC_PINCON ->PINSEL1 |= _BV(14) | _BV(16);
 
   // A/D Control Register
   //  1 in bit 0 - Select AD0.0 to be sampled
+  //  1 in bit 1 - Select AD0.1 to be sampled
   //  0 in bits 15:8 - Don't divide clock
   //  0 in bit 16 - Disable burst mode
   //  1 in bit 21 - Not in power-down mode
   //  0 in bits 26:24 - don't start a conversion yet
-  LPC_ADC ->ADCR = _BV(0) | _BV(21);
+  LPC_ADC ->ADCR = _BV(0) | _BV(1) | _BV(21);
 
   // A/D Interrupt Enable Register
   //  1 in bit 0 - Interrupt when conversion on ADC channel 0 completes
-  LPC_ADC ->ADINTEN = _BV(0);
+  //  1 in bit 1 - Interrupt when conversion on ADC channel 1 completes
+  LPC_ADC ->ADINTEN = _BV(0) | _BV(1);
 
   NVIC_EnableIRQ(ADC_IRQn);
 
-  // Start conversion
-  LPC_ADC ->ADCR |= _BV(24);
+  // Start burst mode
+  LPC_ADC ->ADCR |= _BV(16);
 
   while (1)
     ;

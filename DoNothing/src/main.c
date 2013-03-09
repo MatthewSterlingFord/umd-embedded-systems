cb7ffc223a084374772f1b5116548e9b6ce5feb1
George Pittarelli <g.pittarelli@gmail.com>
2013-03-08 19:54:38 -0500

    Add DoNothing project. Name self-explanatory

---
+++ DoNothing/src/main.c
@@ -0,0 +1,27 @@
+/*
+ ===============================================================================
+ Name        : main.c
+ Author      : 
+ Version     :
+ Copyright   : Copyright (C) 
+ Description : main definition
+ ===============================================================================
+ */
+
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include <cr_section_macros.h>
+#include <NXP/crp.h>
+
+// Variable to store CRP value in. Will be placed automatically
+// by the linker when "Enable Code Read Protect" selected.
+// See crp.h header for more information
+__CRP const unsigned int CRP_WORD = CRP_NO_CRP;
+
+int main(void) {
+  while (1)
+    ;
+  return 0;
+}

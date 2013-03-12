c07adcd4413098f0f439a05a53808b6133d762d1
George Pittarelli <g.pittarelli@gmail.com>
2013-03-12 00:53:08 -0400

    DMA's a square or sine wave to DAC

--- AnalogOutDMA/src/main.c
+++ AnalogOutDMA/src/main.c
@@ -26,6 +26,13 @@ __CRP const unsigned int CRP_WORD = CRP_NO_CRP;
 // DMA DAC Buffer
 __attribute__ ((section(".ahb_ram")))
 static uint32_t dac_dma_buffer[DMA_BUFFER_LEN] = {
+    0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0,
+    0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0,
+    0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0,
+    0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0,
+    0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0, 0, 0xFFC0,
+    0, 0xFFC0, 0, 0xFFC0
+    /*
     0x8000, 0x8C80, 0x9900, 0xA500, 0xB100, 0xBC40,
     0xC700, 0xD100, 0xDA40, 0xE2C0, 0xEA40, 0xF0C0, 0xF600, 0xFA40, 0xFD40,
     0xFF40, 0xFFC0, 0xFF40, 0xFD40, 0xFA40, 0xF600, 0xF0C0, 0xEA40, 0xE2C0,
@@ -34,7 +41,7 @@ static uint32_t dac_dma_buffer[DMA_BUFFER_LEN] = {
     0x15C0, 0x0F40, 0x0A00, 0x05C0, 0x02C0, 0x00C0, 0x0040, 0x00C0, 0x02C0,
     0x05C0, 0x0A00, 0x0F40, 0x15C0, 0x1D40, 0x25C0, 0x2F00, 0x3900, 0x43C0,
     0x4F00, 0x5B00, 0x6700, 0x7380
-    /*
+
     0x8000, 0x8C80, 0x9900, 0xA500, 0xB100, 0xBC40,
     0xC700, 0xD100, 0xDA40, 0xE2C0, 0xEA40, 0xF0C0, 0xF600, 0xFA40, 0xFD40,
     0xFF40, 0xFFC0, 0xFF40, 0xFD40, 0xFA40, 0xF600, 0xF0C0, 0xEA40, 0xE2C0,
@@ -86,10 +93,13 @@ int main(void) {
   LPC_SC ->CLKSRCSEL = 1;
 
   // PLL 0 Setup
-  // M(multipler) value is 96 (so we write 95)
-  // N(divider) value is 6 (so we write 5)
-  // This will yield: F_CCO = (2 * 96 * 12MHz)/6 = 384MHz
-  LPC_SC ->PLL0CFG = (96 - 1) | ((6 - 1) << 16);
+  // M(multipler) value is 147
+  // N(divider) value is 8
+  // F_CCO = (2 * 147 * 12MHz)/8 = 441MHz
+  LPC_SC ->PLL0CFG = (147 - 1) | ((8 - 1) << 16);
+
+  // F_CCO = (2 * 96 * 12MHz)/6 = 384MHz
+  //LPC_SC ->PLL0CFG = (96 - 1) | ((6 - 1) << 16);
 
   // Enable PLL0
   LPC_SC ->PLL0CON = 0x01;
@@ -108,8 +118,8 @@ int main(void) {
   LPC_SC ->PLL0FEED = 0x55;
 
   // Set clock divider to 12, so final running clock speed will be:
-  // (384 / 12) = 32MHz
-  LPC_SC ->CCLKCFG = (12 - 1);
+  // (441 / 100) = 4.41MHz
+  LPC_SC ->CCLKCFG = (100 - 1);
 
   // Peripheral power
   //  Power ADC (bit 12)
@@ -119,7 +129,7 @@ int main(void) {
 
   // Choose undivided peripheral clock for:
   //                    ADC (CLK), DAC (CLK/8)
-  LPC_SC ->PCLKSEL0 |= (1 << 22) | (3 << 24);
+  LPC_SC ->PCLKSEL0 |= (1 << 22) | (1 << 24);
 
   // Setup IO pins (not used currently, handy to keep around)
   LPC_GPIO0 ->FIODIR = _BV(22);
@@ -155,7 +165,7 @@ int main(void) {
   // DAC Counter Value
   //  32MHz / 8 = 4MHz
   //  4MHz / 0xffff = 61.04...Hz
-  LPC_DAC->DACCNTVAL = 6205;
+  LPC_DAC->DACCNTVAL = 49;
 
   // Setup DMA linked list
   dmaLLNode.sourceAddr = (unsigned long int) dac_dma_buffer;
@@ -165,12 +175,12 @@ int main(void) {
   //  Transfer size: 64 (DMA_BUFFER_LEN, bits 11:0)
   //  Source burst size: 1 (0 (default), bits 14:12)
   //  Destination burst size: 1 (0 (default), bits 17:15)
-  //  Source transfer width: word (2, bits 20:18)
-  //  Destination transfer width: word (2, bits 23:21)
+  //  Source transfer width: halfword (1, bits 20:18)
+  //  Destination transfer width: halfword (1, bits 23:21)
   //  Source increment: increment (1, bit 26)
   //  Destination increment: don't increment (0, bit 27)
   //  Terminal count interrupt: disabled (0, bit 31)
-  dmaLLNode.dmaControl = DMA_BUFFER_LEN | (2 << 18) | (2 << 21) | _BV(26);
+  dmaLLNode.dmaControl = DMA_BUFFER_LEN | (1 << 18) | (1 << 21) | _BV(26);
 
   // DMA Channel 0, initialize to linked list for initial DMA
   LPC_GPDMACH0->DMACCSrcAddr = dmaLLNode.sourceAddr;
@@ -184,13 +194,7 @@ int main(void) {
   //  Destination peripheral: DAC (7, bits  10:6)
   //  Transfer Type: memory-to-peripheral (1, bits 13:11)
   LPC_GPDMACH0->DMACCConfig = (7 << 6) | (1 << 11);
-
   LPC_GPDMACH0->DMACCConfig |= _BV(0);
-
-  NVIC_EnableIRQ(ADC_IRQn);
-
-  // Start ADC Burst Mode (bit 16)
-  LPC_ADC ->ADCR |= _BV(16);
 
   volatile int i = 0;
   for (;;) {
